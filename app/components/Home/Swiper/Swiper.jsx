f433b41c1d4c0e179958d8a43cdcba0e4c575ac8
= <=>
2025-02-03 15:41:39 +0200

    update

--- app/components/Home/Swiper/Swiper.jsx
+++ app/components/Home/Swiper/Swiper.jsx
@@ -32,16 +32,10 @@ export default function Swiper() {
     <div className={styles.container}>
       <section className={styles.swiper} key={item?.id}>
         {item?.video ?(
-        <div dangerouslySetInnerHTML={{ __html: `
-          <video
-            loop
-            muted
-            autoplay
-            playsinline
-            src="${item?.video}"
-            class="${styles.video}"
-          />,
-        ` }}></div>
+        <video className={styles.video} playsInline={true}  webkit-playsinline="true"
+        style={{ width: '100%', height: 'auto' }}  autoPlay={true} muted={true} loop={true} controls={false} >
+        <source src={item?.video} />
+        </video>
         ):
         (
           <Image className={styles.video} src={item?.image}  alt={item?.name}  />
