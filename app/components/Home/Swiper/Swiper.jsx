66910602264a28c080dd9968d1987ad31da08576
= <=>
2025-02-03 15:48:52 +0200

    update

--- app/components/Home/Swiper/Swiper.jsx
+++ app/components/Home/Swiper/Swiper.jsx
@@ -32,8 +32,8 @@ export default function Swiper() {
     <div className={styles.container}>
       <section className={styles.swiper} key={item?.id}>
         {item?.video ?(
-        <video className={styles.video} playsInline={true}  webkit-playsinline="true"
-        style={{ width: '100%', height: 'auto' }}  autoPlay={true} muted={true} loop={true} controls={false} >
+        <video className={styles.video} playsInline="playsinline"  webkit-playsinline="true"
+        autoPlay="autoplay" muted="true" loop="true" controls={false} >
         <source src={item?.video} />
         </video>
         ):
